6954197c76824fa018430b8a88c5d0d599f32a3f
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-02-04 11:39:16 -0300

    create cycle proviber

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
 import { defaultTheme } from './styles/themes/default'
-import { GlogalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
-      <GlogalStyle />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
